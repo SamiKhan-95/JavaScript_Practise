bf2f379b3a7b1c0dec3cafc8f01608481128fa9a
Samikhan <samikhan2872@gmail.com>
2021-03-07 09:05:03 +0500

    added row1 edit option

--- table.js
+++ table.js
@@ -1,18 +1,3 @@
-// document.write("<div style = 'width:800px'>");
-// document.write("<table border ='1'><form>");
-// document.write("<form><tr><th>S.No</th><th>First Name</th><th>Last Name</th>");
-// document.write("<th>Class</th><th>Action</th></tr>");
-// for (var i = 1; i <= 4; i++){
-//     document.write("<tr><td>"+i+"</td>");
-//     document.write("<td><input type = 'text'></td>");
-//     document.write("<td><input type = 'text'></td>");
-//     document.write("<td><input type = 'text'></td>");
-//     document.write("<td><button onclick='edit()'>Edit</button><button onclick='delEntireRow(1)'>Del</button></td>");
-//     document.write("</tr>")
-// }
-// document.write("</form>")
-// document.write("</table>");
-// document.write("</div><br/>");
 // document.write("<input type='submit' value = Submit id = 'sub'>");
 function delEntireRow(flag){
     switch(flag){
@@ -33,6 +18,20 @@ function delEntireRow(flag){
 function edit(flag){
     switch(flag){
         case 'row1':
-            document.getElementById('row1').style.display = 'none';
+            document.write("<div style = 'width:800px'>");
+            document.write("<table border ='1'><form>");
+            document.write("<form><tr><th>S.No</th><th>First Name</th><th>Last Name</th>");
+            document.write("<th>Class</th><th>Action</th></tr>");
+            document.write("<tr>")
+            document.write("<td>"+1+"</td>")
+            document.write("<td><input type = 'text'></td>");
+            document.write("<td><input type = 'text'></td>");
+            document.write("<td><input type = 'text'></td>");
+            document.write("<td><button>OK</button></td>")
+            document.write("</tr>")
+            document.write("<tr><td>2</td><td>Ahmed</td><td>Ali</td><td>7</td><td><button>OK</button></td></tr>")
+            document.write("<tr><td>3</td><td>Hamza</td><td>Khan</td><td>2</td><td><button>OK</button></td></tr>")
+            document.write("<tr><td>4</td><td>Behzad</td><td>Mirza</td><td>8</td><td><button>OK</button></td></tr>")
+            break
     }
 }
